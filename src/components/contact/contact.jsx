6726470c7d392ec373d6cee2925c046f0fea50c3
Mark Dave Lorejo <dave.lorejo69@gmail.com>
2024-03-19 14:52:05 +0800

    added initial divs

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,11 +1,29 @@
-import './contact.css'
+import "./contact.css";
+import React from "react";
 
-export default function Contact(){
-    return(
-        <>
-        <div className='contact-container'>
-            <h2>Contact</h2>
+export default function Contact() {
+
+    const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <div className="contact-container">
+        <h2>Contact</h2>
+        <div className="up">
+          <div className="up-left">
+            <div className="up-left-content"></div>
+            <div className="up-left-content"></div>
+          </div>
+          <div className="up-left">
+            <div className="up-left-content"></div>
+            <div className="up-left-content"></div>
+          </div>
         </div>
-        </>
-    );
+        <div className="down"></div>
+        <div className="footer">
+          <p>© {currentYear} Mark Dave Lorejo</p>
+        </div>
+      </div>
+    </>
+  );
 }
